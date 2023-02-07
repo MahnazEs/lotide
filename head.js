4d081642f6119c779209e36926240bae51be9bed
“MahnazEs” <esmaeilymahnaz@gmail.com>
2023-02-06 22:01:09 -0800

    add head.js with return first element

---
+++ head.js
@@ -0,0 +1,21 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log("✅✅✅ " + `${actual} === ${expected}`);
+  } else {
+    console.log("🛑🛑🛑 " + `${actual} !== ${expected}`);
+  }
+};
+
+const head = function (data) {
+  if (data) {
+    const firstElement = data.shift();
+    return firstElement;
+  }else{
+    return undefined;
+  }
+}
+
+// TEST CODE
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
