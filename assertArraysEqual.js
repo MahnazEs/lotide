dd65b0898c8c8adb4811e29a8972d8aac8bbb416
“MahnazEs” <esmaeilymahnaz@gmail.com>
2023-02-08 19:42:01 -0800

    add assertArraysEqual.js

---
+++ assertArraysEqual.js
@@ -0,0 +1,30 @@
+
+const eqArrays = function (arr1, arr2) {
+  let lenArr1 = arr1.length;
+  let lenArr2 = arr1.length;
+  let result = true;
+  if (lenArr1 > 0 && lenArr2 > 0) {
+    if (lenArr1 === lenArr2) {
+      for (let i = 0; i < arr1.length; i++) {
+        if (!(arr1[i] === arr2[i])) {
+          result = false;
+        }
+      }
+      return result;
+    }
+    return false;
+  }
+  console.log("please enter data in Array!");
+};
+
+const assertArraysEqual = function(array1, array2) {
+  if (eqArrays(array1, array2)) {
+    console.log(`✅✅✅ ${array1} === ${array2}`);
+  } else {
+    console.log(`🛑🛑🛑 ${array1} !== ${array2}`);
+  }
+};
+
+console.log(assertArraysEqual([3, 2, 1], [3, 2, 1]));
+
+
