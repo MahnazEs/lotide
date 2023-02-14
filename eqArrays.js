2355bcff787c3bc18014e2ea0cc71b3fa821c591
“MahnazEs” <esmaeilymahnaz@gmail.com>
2023-02-13 17:15:54 -0800

    refactore eqArrays.js by module

--- eqArrays.js
+++ eqArrays.js
@@ -1,24 +1,48 @@
+
+
+// FUNCTION IMPLEMENTATION
+/* const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log("✅✅✅ " + `${actual} === ${expected}`);
+  } else {
+    console.log("🛑🛑🛑 " + `${actual} !== ${expected}`);
+  }
+}; */
+
+
+
+
 const eqArrays = function (arr1, arr2) {
   let lenArr1 = arr1.length;
   let lenArr2 = arr1.length;
   let result = true;
-  if (lenArr1 > 0 && lenArr2 > 0) {
-    if (lenArr1 === lenArr2) {
-      for (let i = 0; i < arr1.length; i++) {
-        if (!(arr1[i] === arr2[i])) {
-          result = false;
-        }
-      }
-      return result;
-    }
+  if (lenArr1 !== lenArr2) {
     return false;
   }
-  console.log("please enter data in Array!");
+
+  for (let i = 0; i < arr1.length; i++) {
+    if (Array.isArray(arr1[i]) && (eqArrays(arr1[i], arr2[i]) === false)) {
+      return false;
+    }
+    if (!(Array.isArray(arr1[i])) && arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
 };
 
 
 
-console.log(eqArrays([1, 2, 3], [3, 2, 1]));
+
+//TEST CODE
+//assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
+module.exports = eqArrays;
+
+
+
+
+
+
 
 
 /* eqArrays([1, 2, 3], [3, 2, 1]) // => false
